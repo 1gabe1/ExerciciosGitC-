6ecbf5460cc3b5afcaa6fe05196878fc9b9368ce
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-08-25 22:59:16 -0300

    Cadastro Simples

---
+++ CodigosTps/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CodigosTps {
+    internal class Program {
+        static void Main(string[] args) {
+
+           
+            
+            
+            
+
+            Console.WriteLine("Digite seu Nome completo: ");
+            String nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu telefone: ");
+            String telefone = Console.ReadLine();
+
+            Console.WriteLine("Digite seu e-mail de usuario: ");
+            String email = Console.ReadLine();
+
+
+            Console.WriteLine("===== DADOS DO USUARIO CADASTRADO =====");
+
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("Telefone: " + telefone);
+            Console.WriteLine("E-mail: " + email);
+
+            
+
+
+        }
+    }
+}
