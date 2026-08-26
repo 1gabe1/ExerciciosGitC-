6132078b6fc88115dea8479aff3eb44a8b116666
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-08-25 23:12:52 -0300

    Conversor de Temperatura

---
+++ Codigostps2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Codigostps2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite a temperatura em Graus Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            Double Fahrenheit = Fahrenheit = celsius * 9 / 5 + 32;
+            Console.WriteLine($"Valor de Celsius para Fahrenheit: {Fahrenheit:F2} °F");
+
+            Double Kelvin = Kelvin = celsius + 273.15;
+            Console.WriteLine($"Valor de Celsius para Kelvin: {Kelvin:F2} K");
+
+        }
+    }
+}
