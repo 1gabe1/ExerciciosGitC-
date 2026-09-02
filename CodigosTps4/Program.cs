b684e2dbc094abe220a13cbc19a8912b49f7dcb0
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-01 22:12:07 -0300

    Verificador de numeros Impares/Pares e README

---
+++ CodigosTps4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CodigosTps4 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um Numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            if (numero % 2 == 0) {
+                Console.WriteLine($"Numero Par: {numero}");
+
+
+            }
+            else {
+                Console.WriteLine($"Numero Impar: {numero}");
+
+            }
+
+        }
+    }
+}
