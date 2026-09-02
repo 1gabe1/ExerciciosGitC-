423bb2abbc68dec8a81abf73257d8ff89432301c
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-01 22:30:59 -0300

    Verificando notas de provas!

---
+++ CodigosTps5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CodigosTps5 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insira uma nota de Prova: ");
+            double notaProva = double.Parse(Console.ReadLine());
+
+            if (notaProva >= 0 && notaProva <= 10) {
+
+                if (notaProva <= 4.9) {
+                    Console.WriteLine("Insuficiente..");
+
+                }
+                else if (notaProva <= 6.9) {
+                    Console.WriteLine("Regular :3");
+
+                }
+                else if (notaProva <= 8.9) {
+                    Console.WriteLine("Bomm :>");
+
+                }
+                else {
+                    Console.WriteLine("Excelente");
+
+                }
+
+            }
+            else {
+                Console.WriteLine("ERRO - NOTA INVALIDA");
+
+
+
+            }
+
+            
+        }
+    }
+}
