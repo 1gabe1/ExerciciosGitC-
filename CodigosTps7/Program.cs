1c55e5155d407f88ff10acc5d654f248b11f8fe8
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-03 23:22:31 -0300

    Contagem Regressiva

---
+++ CodigosTps7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CodigosTps7 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Informe um Numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0) {
+                Console.Write(numero);
+               
+                if (numero != 0) {
+
+                    Console.Write(", ");
+                }
+
+                numero--;
+            }
+
+
+        }
+    }
+}
