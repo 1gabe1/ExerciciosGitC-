f74f2c5c6ec8bdeec8a5c002a37756a188e04822
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-10 23:24:03 -0300

    Calculo de idade precisa

---
+++ CodigosTps10/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace CodigosTps10 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Informe sua data de Nascimento: ");
+
+            DateTime dataNas = DateTime.Parse(Console.ReadLine());
+
+            DateTime datahoje = DateTime.Now;
+
+            int anos = datahoje.Year - dataNas.Year;
+
+            if (dataNas.Month > datahoje.Month) {
+                
+                anos--;
+
+            }
+            else if (dataNas.Month == datahoje.Month) {
+
+                if (dataNas.Day > datahoje.Day) {
+
+                    anos--;
+                   
+
+                }
+               
+                
+            }
+
+            int meses = datahoje.Month - dataNas.Month;
+
+            if (dataNas.Month > datahoje.Month) {
+                meses = meses + 12;
+
+            }
+
+            if (dataNas.Day > datahoje.Day ) {
+                meses--;
+            
+            }
+
+            int dias = datahoje.Day - dataNas.Day;
+
+            if (dias < 0) {
+
+                int mesAnterior = datahoje.Month - 1;
+
+                if (mesAnterior == 0) {
+                    mesAnterior = 12;
+                }
+
+                int anoMesAnterior = datahoje.Year;
+
+                if (datahoje.Month == 1) {
+                    anoMesAnterior--;
+                }
+
+                dias = dias + DateTime.DaysInMonth(anoMesAnterior, mesAnterior);
+            }
+
+            Console.WriteLine($"Você está com {anos} anos, {meses} meses e {dias} dias.");
+
+
+        }
+    }
+}
