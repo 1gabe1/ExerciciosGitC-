e6b4c419ab5919a6a3b90b83f49d72cdb018eb5a
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-10 23:25:36 -0300

    Dias até o Próximo Aniversário

---
+++ CodigosTps11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CodigosTps11 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Informe sua data de nascimento: ");
+            DateTime Nascimento = DateTime.Parse(Console.ReadLine());
+
+            DateTime dataDehoje = DateTime.Now;
+
+            DateTime aniversarioProx = new DateTime(
+                dataDehoje.Year,
+                Nascimento.Month,
+                Nascimento.Day
+            );
+
+            if (aniversarioProx < dataDehoje) {
+
+                aniversarioProx = aniversarioProx.AddYears(1);
+
+            }
+
+            TimeSpan diferenca = aniversarioProx - dataDehoje;
+
+            int dias = diferenca.Days;
+
+            Console.WriteLine($"Faltam {dias} dias para o seu próximo aniversário.");
+
+        }
+    }
+}
