e3f0979299735fe63cb566923f5d3a54b17d0ac7
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-10 23:21:51 -0300

    Jogo de Adivinhação

---
+++ CodigosTps9/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CodigosTps9 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Random random = new Random();
+
+            int numeroSecret = random.Next(1, 101);
+
+            int palpite = 0;
+
+            while (palpite != numeroSecret ) {
+
+                Console.WriteLine("Digite um numero: ");
+                palpite = int.Parse(Console.ReadLine());
+
+                if (palpite > numeroSecret) {
+                    Console.WriteLine("O número secreto etá para baixo..");
+
+                }
+                else if (palpite < numeroSecret) {
+                    Console.WriteLine("O numero secreto está para cima");
+
+                }
+                else {
+                    Console.WriteLine("ACERTOUUU!!");
+                
+                }
+            }
+        }
+    }
+}
