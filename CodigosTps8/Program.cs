cef51ac6ce5ea3974ad89e9afa7c44f0e6b0025a
Gabriel Crepaldi <gabrielcre89@gmail.com>
2026-09-10 23:20:31 -0300

    Tabuada Interativa

---
+++ CodigosTps8/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CodigosTps8 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insira um Numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int contador = 1;
+
+            while (contador <= 10 ) {
+
+                Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+
+                contador++;
+
+                
+            }
+
+
+
+        }
+    }
+}
